332dfd491a0448c9467b485c651a48581e80b54b
kranthi chapidi <kranthi chapidi>
2016-09-15 21:27:32 -0400

    Project Setup

---
+++ app/src/main/java/com/example/kranthi/assignment1/AddSubMulDiv.java
@@ -0,0 +1,13 @@
+package com.example.kranthi.assignment1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddSubMulDiv extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_sub_mul_div);
+    }
+}
